3b7954faaa0dee795e2059775654317e12ad8f39
dekyit <dekyitenz@gmail.com>
2023-11-26 12:53:36 -0500

    push changes

--- Front-End/frontend/src/App.js
+++ Front-End/frontend/src/App.js
@@ -1,16 +1,18 @@
 
 import './App.css';
 import ProductPage from './components/ProductPage';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Hero from './components/Hero';
 
 function App() {
   return (
     <div className="App">
-     <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-<ProductPage/>
-    
-
+     
+      <Navbar />
+      <Hero/>
+      <ProductPage />
+      <Footer/>
     </div>
   );
 }
