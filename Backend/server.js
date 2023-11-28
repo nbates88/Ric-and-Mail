9ccb004e95b6fa01771edbc07ef4aabb99ab2d9c
dekyit <dekyitenz@gmail.com>
2023-11-27 19:43:51 -0500

    no changes

--- Backend/server.js
+++ Backend/server.js
@@ -1,17 +1,18 @@
-
 require('dotenv').config({path: '.env'});
 
+const express = require("express")
 const {Sequelize, DataTypes} = require('sequelize');
+
+const app = express()
 
 const sequelize = new Sequelize( process.env.PG_URI,{
 
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  host:"127.0.0.1",
+  host:"3080",
   dialect:'postgres',
-});
-
+})
 
 const User = sequelize.define('User', {
   username: {
@@ -31,5 +32,7 @@ sequelize.authenticate()
 
 sequelize.sync();
 
-
+app.listen(3080, () => {
+  console.log('nomming at port', 3080);
+})
 
