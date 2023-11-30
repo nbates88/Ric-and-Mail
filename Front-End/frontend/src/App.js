e257c8928dc09b8ebb04c24a1c3cca72ab6ee571
dekyit <dekyitenz@gmail.com>
2023-11-30 10:00:48 -0500

    added product details page

--- Front-End/frontend/src/App.js
+++ Front-End/frontend/src/App.js
@@ -1,23 +1,24 @@
 import './App.css';
-import ProductPage from './places/shop';
+import Shop from './places/Shop';
+import Home from './places/Home';
+import About from './places/About';
+import Contact from './places/Contact';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Hero from './components/Hero';
-import home from './places/home'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-     
-      <Navbar />
-        <BrowserRouter>
+        <BrowserRouter >
+          <Navbar/>
           <Routes>
-            <Route exact path="/" element={<home/>}></Route>
-            <Route exact path="/shop"></Route>
-            <Route exact path="/aboutUs"></Route>
-            <Route exact path="/contactUs"></Route>
+            <Route path="/home" element={<Home/>}/>
+            <Route exact path="/Shop" element={<Shop/>}/>
+            <Route exact path="/About" element={<About/>}/>
+            <Route exact path="/Contact" element={<Contact/>}/>
           </Routes>
         </BrowserRouter>
     </div>
---
+++ Front-End/frontend/src/places/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Contact() {
+  return (
+    <div>
+      <p>put fake contact info here</p>
+    </div>
+  )
+}
+
+export default Contact;
