c6c0fb1c52df0dc18b217fa1660b8df605d44cc7
dekyit <dekyitenz@gmail.com>
2023-11-30 10:04:20 -0500

    new branch

--- Front-End/frontend/src/App.js
+++ Front-End/frontend/src/App.js
@@ -1,19 +1,41 @@
 import './App.css';
+<<<<<<< HEAD
 import Shop from './places/Shop';
 import Home from './places/Home';
 import About from './places/About';
 import Contact from './places/Contact';
+=======
+import ProductPage from './components/ProductPage';
+// import Form from './components/Form';
+import ProductPage from './places/shop';
+>>>>>>> c6060eadb6c70050ea94bf5f4ad6f98f712ecbd2
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Hero from './components/Hero';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+<<<<<<< HEAD
 import React from 'react';
+=======
+>>>>>>> c6060eadb6c70050ea94bf5f4ad6f98f712ecbd2
 
 function App() {
   return (
     <div className="App">
+<<<<<<< HEAD
         <BrowserRouter >
           <Navbar/>
+=======
+     <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    {/* <Form/> */}
+<ProductPage/>
+    
+
+     
+      <Navbar />
+        <BrowserRouter>
+>>>>>>> c6060eadb6c70050ea94bf5f4ad6f98f712ecbd2
           <Routes>
             <Route path="/home" element={<Home/>}/>
             <Route exact path="/Shop" element={<Shop/>}/>
@@ -21,6 +43,7 @@ function App() {
             <Route exact path="/Contact" element={<Contact/>}/>
           </Routes>
         </BrowserRouter>
+
     </div>
   );
 }
